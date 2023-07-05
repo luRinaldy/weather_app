c7968fc50ec13454e451d0da69acaf3ee59e8c3c
luRinaldy <rinaldypotesmartha@gmail.com>
2023-07-04 23:18:22 -0500

    first release to production

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-let now = new Date();
-let h6 = document.querySelector("h6");
-
-let Hours = now.getHours();
-
-let Minutes = now.getMinutes(); // 0,1,2, 12
-// 0, 1, 2
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thurday",
-  "Friday",
-  "Saturday",
-];
-let Day = days[now.getDay()];
-
-h6.innerHTML = `${Day}, ${Hours}:${Minutes}`;
-
 function showTemperature(response) {
   console.log(response.data);
+
+  let now = new Date();
+  let h6 = document.querySelector("h6");
+
+  let Hours = now.getHours();
+
+  let Minutes = now.getMinutes();
+  // 0, 1, 2
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let Day = days[now.getDay()];
+
+  h6.innerHTML = `Last update: ${Day}, ${Hours}:${Minutes}`;
 
   document.querySelector("h1").innerHTML = response.data.name;
 
